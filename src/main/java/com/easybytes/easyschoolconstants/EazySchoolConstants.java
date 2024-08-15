a8773456e810ff92f016a5ddab0848df5087aef4
codewithmangesh <mangeshsonawane8887@gmail.com>
2024-08-15 12:43:05 +0530

    tetsing

--- src/main/java/com/easybytes/easyschoolconstants/EazySchoolConstants.java
+++ src/main/java/com/easybytes/easyschoolconstants/EazySchoolConstants.java
@@ -7,4 +7,5 @@ public interface EazySchoolConstants {
     public static final String CLOSE = "Close";
     public static final String STUDENT_ROLE = "STUDENT";
     public static final String ADMIN_ROLE = "ADMIN";
+    public static final String USER_ROLE = "USER";
 }
